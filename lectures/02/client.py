c3b1065faeec71d1073f4756fd1ec358ab59430a
Nicklason <nicklas.marc.pedersen@gmail.com>
2023-09-09 17:48:31 +0200

    connect to multiple hdfs name nodes

--- lectures/02/client.py
+++ lectures/02/client.py
@@ -1,10 +1,14 @@
-from hdfs import InsecureClient 
+from hdfs import InsecureClient
 
-
-HDFS_URL: str = "http://simple-hdfs-namenode-default-0"
-HDFS_PORT: str = "9870"
-HDFS_USERNAME: str = "stackable"
-
+HDFS_HOSTS = [
+    "http://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:9870",
+    "http://simple-hdfs-namenode-default-1.simple-hdfs-namenode-default:9870"
+]
+HDFS_USER_NAME: str = "stackable"
 
 def get_hdfs_client() -> InsecureClient:
-    return InsecureClient(f"{HDFS_URL}:{HDFS_PORT}", user=HDFS_USERNAME)
+    url = ""
+    for host in HDFS_HOSTS:
+        url += host + ";"
+    url = url[:-1]
+    return InsecureClient(url, user=HDFS_USER_NAME)
