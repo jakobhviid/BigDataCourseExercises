ef940728fb9fd6302d4b7fb101d53f4b6a9f71c2
Nicklason <nicklas.marc.pedersen@gmail.com>
2023-09-09 17:49:12 +0200

    update simple client

--- lectures/02/simple-client.py
+++ lectures/02/simple-client.py
@@ -1,30 +1,33 @@
-from hdfs import InsecureClient
+from client import get_hdfs_client
 
 def main():
+    # Create a client
+    client = get_hdfs_client()
 
-    # Create an insecure client that works when HDFS has security turned off
-    client = InsecureClient('http://<deployment name>:<node port>', user='root') # TODO: check up name and port 
+    print("Reading contents of file /alice-in-wonderland.txt:")
 
-    first_line: str = ''
+    try:
+        # Reading a file, using a delimiter makes it return a list
+        with client.read('/alice-in-wonderland.txt', encoding='utf-8', delimiter='\n') as reader:
+            for line in reader:
+                print(line)
+    except Exception as e:
+        print("Failed to read file: " + e.message)
 
-    # Reading a file, using a delimiter makes it return a list
-    with client.read('/alice-in-wonderland.txt', encoding='utf-8', delimiter='\n') as reader:
-        for line in reader:
-            print(line)
-            first_line = line
-            # Only first line is interesting for now
-            break
-        
+    print("Creating file at /write.txt...")
+
     # One line writing to a file
     # client.write('/write2.txt', first_line, encoding='utf-8', overwrite=True)
 
     # Multi line writing
     with client.write('/write.txt', encoding='utf-8', overwrite=True) as writer:
-        writer.write(first_line)
-        writer.write('\n')
-        writer.write(first_line)
-        writer.write('\n')
-
+        writer.write("this is one line of text\n")
+        writer.write("this is another line of text\n")
+    
+    print("Reading contents of file /write.txt:")
+    with client.read('/write.txt', encoding='utf-8', delimiter='\n') as reader:
+        for line in reader:
+            print(line)
 
 if __name__ == "__main__":
     main()
