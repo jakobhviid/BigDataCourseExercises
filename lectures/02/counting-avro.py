261d79b71859e3f8266a7eb55637cc41468cccfe
Anders Launer Bæk-Petersen <anders@launer.dk>
2023-09-10 23:48:32 +0200

    linting and changes `counting-parquet.py` to have many rows.

--- lectures/02/counting-avro.py
+++ lectures/02/counting-avro.py
@@ -1,20 +1,20 @@
-
 from collections import Counter
 from hdfs.ext.avro import AvroReader, AvroWriter
 
 from client import get_hdfs_client
 
+
 def main():
     client = get_hdfs_client()
 
     # Create an AvroWriter instance with the client and file name
-    with client.read("/alice-in-wonderland.txt", encoding="utf-8") as reader, AvroWriter(
-        client, "/word-count.avro", overwrite=True
-    ) as writer:
+    with client.read(
+        "/alice-in-wonderland.txt", encoding="utf-8"
+    ) as reader, AvroWriter(client, "/word-count.avro", overwrite=True) as writer:
         wordcount = Counter(reader.read().split()).most_common(10)
 
         # Restructure the wordcount structure, so it now is a an array of objects, objects that contain a word and count key
-        for (key, count) in wordcount:
+        for key, count in wordcount:
             writer.write({"word": key, "count": count})
 
     # Read the written file
@@ -27,6 +27,7 @@ def main():
         print("\n")
         # Print a list of the data
         print(list(reader))
+
 
 if __name__ == "__main__":
     main()
--- lectures/02/counting-parquet.py
+++ lectures/02/counting-parquet.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 from client import get_hdfs_client
 
+
 def main():
     client = get_hdfs_client()
-    
-    with client.read("/alice-in-wonderland.txt", encoding="utf-8") as reader:
-        wordcount = Counter(reader.read().split()).most_common(10)
 
-        df = pd.DataFrame(wordcount, columns=["word", "count"])
+    with client.read("/alice-in-wonderland.txt", encoding="utf-8") as reader:
+        wordcount = Counter(reader.read().split())
+
+        df = pd.DataFrame.from_dict(dict(wordcount), orient="index").transpose()
         df.to_parquet("/word-count.parquet")
 
         client.upload("/word-count.parquet", "/word-count.parquet", overwrite=True)
@@ -17,6 +18,7 @@ def main():
     with client.read("/word-count.parquet", encoding="utf-8") as reader:
         df = pd.read_parquet("/word-count.parquet")
         print(df)
+
 
 if __name__ == "__main__":
     main()
