5e8530051e8ff9eb58dde0ce419983e5de5200b7
Anders Launer Bæk-Petersen <anders@launer.dk>
2023-09-10 22:20:32 +0200

    Just condensed the operation of HDFS_HOSTS.

--- lectures/02/client.py
+++ lectures/02/client.py
@@ -1,14 +1,11 @@
 from hdfs import InsecureClient
 
-HDFS_HOSTS = [
+HDFS_HOSTS: list[str] = [
     "http://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default:9870",
-    "http://simple-hdfs-namenode-default-1.simple-hdfs-namenode-default:9870"
+    "http://simple-hdfs-namenode-default-1.simple-hdfs-namenode-default:9870",
 ]
 HDFS_USER_NAME: str = "stackable"
 
+
 def get_hdfs_client() -> InsecureClient:
-    url = ""
-    for host in HDFS_HOSTS:
-        url += host + ";"
-    url = url[:-1]
-    return InsecureClient(url, user=HDFS_USER_NAME)
+    return InsecureClient(";".join(HDFS_HOSTS), user=HDFS_USER_NAME)
