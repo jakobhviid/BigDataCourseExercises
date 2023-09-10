8bbf8af7d1fa8646e5aeaa1df00cf66aaad70377
Anders Launer Bæk-Petersen <anders@launer.dk>
2023-09-10 23:48:55 +0200

    linting..

--- lectures/02/simple-client.py
+++ lectures/02/simple-client.py
@@ -1,5 +1,6 @@
 from client import get_hdfs_client
 
+
 def main():
     # Create a client
     client = get_hdfs_client()
@@ -8,7 +9,9 @@ def main():
 
     try:
         # Reading a file, using a delimiter makes it return a list
-        with client.read('/alice-in-wonderland.txt', encoding='utf-8', delimiter='\n') as reader:
+        with client.read(
+            "/alice-in-wonderland.txt", encoding="utf-8", delimiter="\n"
+        ) as reader:
             for line in reader:
                 print(line)
     except Exception as e:
@@ -20,14 +23,15 @@ def main():
     # client.write('/write2.txt', first_line, encoding='utf-8', overwrite=True)
 
     # Multi line writing
-    with client.write('/write.txt', encoding='utf-8', overwrite=True) as writer:
+    with client.write("/write.txt", encoding="utf-8", overwrite=True) as writer:
         writer.write("this is one line of text\n")
         writer.write("this is another line of text\n")
-    
+
     print("Reading contents of file /write.txt:")
-    with client.read('/write.txt', encoding='utf-8', delimiter='\n') as reader:
+    with client.read("/write.txt", encoding="utf-8", delimiter="\n") as reader:
         for line in reader:
             print(line)
+
 
 if __name__ == "__main__":
     main()
