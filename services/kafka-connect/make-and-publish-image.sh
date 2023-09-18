514643f7d086c63b6360d89cdabe27d1913fae39
Anders Launer Bæk-Petersen <anders@launer.dk>
2023-09-19 00:17:29 +0200

    foundation works.

--- services/kafka-connect/make-and-publish-image.sh
+++ services/kafka-connect/make-and-publish-image.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 tag=7.3.1
+imagetag=lec03hdfs2
 containername=cp-server-connect-base
 dockerhubacc=anderslaunerbaek
-imagename=$dockerhubacc/$containername:$tag
+imagename=$dockerhubacc/$containername:$imagetag
 
 echo $imagename
 docker build --build-arg="CP_VERSION=$tag" --tag=$imagename  . 
