61f71fde1b96bb0795a2d38524aa8ad1e9f9a73e
ks <svane.kasper20@gmail.com>
2024-08-09 09:41:14 +0200

    task-28/update-scripts Give access to all resources (*)

--- infrastructure/create-users.sh
+++ infrastructure/create-users.sh
@@ -9,6 +9,12 @@ fi
 SERVER_IP=$1
 SERVER="https://${SERVER_IP}:16443"
 
+# Check if MicroK8s is running
+microk8s status --wait-ready || { echo "MicroK8s is not running. Exiting."; exit 1; }
+
+# Enable necessary addons
+microk8s enable rbac dns cert-manager || { echo "Failed to enable addons. Exiting."; exit 1; }
+
 echo "Create kubeconfigs!"
 while read NAMESPACE; do
   ./create-user.sh "$SERVER" "$NAMESPACE"
