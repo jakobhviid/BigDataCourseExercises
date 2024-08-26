6d436906945ae961145b1bb831646a1ae27c51e4
Anders Launer Bæk-Petersen <28479232+anderslaunerbaek@users.noreply.github.com>
2024-08-26 09:52:06 +0200

    Lecture03 (#44)
    * ready to work on lecture 03
    
    * added images to sideload.
    
    * Lecture02 (#43)
    
    * initial commit for lecture 02-
    
    * initial commit for lecture 02-
    
    * making progress
    
    * making progress
    
    * hdfs.
    
    * lecture02 Make sure that the order is PersistentVolume, Deployment, Service and updated the documentation
    
    * lecture02 Updated docs
    
    * resolved alias, kubectl cmds.
    
    * updated hdfs datanodes from deployment to pod resoruces.
    
    * ready to test exercises.
    
    * added cleanup section.
    
    * added nudging to open github issue for bugs.
    
    * lecture02 Added configuration for HDFS cli
    
    * changes ferences from README.md -> README.
    
    ---------
    
    Co-authored-by: Anders Launer Baek-Petersen <anbae@mmmi.sdu.dk>
    
    * ready to work on lecture 03
    
    * added images to sideload.
    
    * staged and push to change to another computer.
    
    * serives are up......
    
    * working on exercises.
    
    * changed the strcuture of the manifest files for hdfs.
    
    * kafka, consumers and producers, registry, ksqldb, and connect modules.
    
    * updated cleanup sections
    
    * bugfix: updated filename in readme.
    
    * lecture03 Update the configuration for the kafka deployments to use configmaps instead in case they have to be changed during runtime
    
    * lecture03 Update documentation for lecture 2
    
    * lecture03 Working on Sqoop
    
    * Update kafka-values.yaml
    
    * lecture03 Can now ingest PostgresSql database into HDFS using Sqoop
    
    * lecture03 Working on Flume
    
    * lecture03 Can now use Flume to ingest data into Kafka
    
    * lecture03 Updated cleanup
    
    * Update documentation
    
    * lecture03 Updated docs for Flume
    
    * lecture03 Updated docs for Flume
    
    * lecture03 Updated docs for Sqoop
    
    * read the exercises.
    
    ---------
    
    Co-authored-by: Kasper Svane <svane.kasper20@gmail.com>

---
+++ lectures/03/hints/client.py
@@ -0,0 +1,40 @@
+from kafka import KafkaProducer, KafkaConsumer
+import json
+from data_model import generate_sample, PackageObj
+
+KAFKA_BROKERS: str = (
+    "strimzi-kafka-bootstrap.kafka:9092"  # <service name>.<namepsace>:<port>
+)
+
+DEFAULT_TOPIC: str = "INGESTION"
+DEFAULT_ENCODING: str = "utf-8"
+DEFAULT_CONSUMER: str = "DEFAULT_CONSUMER"
+
+
+def get_producer() -> KafkaProducer:
+    return KafkaProducer(bootstrap_servers=[KAFKA_BROKERS])
+
+
+def get_consumer(topic: str, group_id: str = None) -> KafkaConsumer:
+    if group_id is None:
+        group_id = DEFAULT_CONSUMER
+    return KafkaConsumer(topic, bootstrap_servers=[KAFKA_BROKERS], group_id=group_id)
+
+
+def send_msg(value, key: str, topic: str, producer: KafkaProducer) -> None:
+    producer.send(
+        topic=topic,
+        key=key.encode(DEFAULT_ENCODING),
+        value=json.dumps(value).encode(DEFAULT_ENCODING),
+    )
+
+
+def produce_msg(sensor_id: int, topic: str, producer: KafkaProducer) -> None:
+    key, value = generate_sample(sensor_id=sensor_id)
+    print(value)
+    send_msg(key=str(key), value=value, topic=topic, producer=producer)
+
+
+def recive_msg(consumer: KafkaConsumer) -> None:
+    for msg in consumer:
+        print(PackageObj(**json.loads(msg.value.decode(DEFAULT_ENCODING))))
---
+++ lectures/03/hints/data_model.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass, field, asdict
+from datetime import datetime
+from uuid import uuid4
+import random
+import json
+
+VALID_SENSOR_IDS: list[int] = [1, 2, 3, 4, 5, 6]
+VALID_TEMPORAL_ASPECTS: list[str] = ["real_time", "edge_prediction"]
+VALID_RANGE: tuple[int] = (-600, 600)
+
+
+def get_uuid():
+    return str(uuid4())
+
+
+@dataclass
+class SensorObj:
+    sensor_id: str
+    modality: float
+    unit: str
+    temporal_aspect: str
+
+    def to_dict(self) -> dict:
+        return asdict(self)
+
+
+@dataclass
+class PackageObj:
+    payload: SensorObj
+    correlation_id: str = field(default_factory=get_uuid)
+    created_at: datetime = field(default_factory=datetime.utcnow)
+    schema_version: int = field(default=1)
+
+    def __post_init__(self):
+        self.payload = (
+            self.str_to_sensor_obj(self.payload)
+            if isinstance(self.payload, str)
+            else self.payload
+        )
+
+    def str_to_sensor_obj(self, x: str) -> SensorObj:
+        return SensorObj(**json.loads(x))
+
+    def to_dict(self):
+        self.created_at = self.created_at.timestamp()
+        self.payload = json.dumps(self.payload.to_dict())
+        return asdict(self)
+
+
+def get_sensor_sample(
+    sensor_id: int = None,
+    modality: int = None,
+    unit: str = "MW",
+    temporal_aspect: str = VALID_TEMPORAL_ASPECTS[0],
+) -> SensorObj:
+    if sensor_id is None:
+        sensor_id = random.choice(VALID_SENSOR_IDS)
+    if modality is None:
+        modality = random.choice(range(VALID_RANGE[0], VALID_RANGE[1] + 1))
+    return SensorObj(
+        sensor_id=sensor_id,
+        modality=modality,
+        unit=unit,
+        temporal_aspect=temporal_aspect,
+    )
+
+
+def generate_sample(sensor_id: int) -> tuple[int, dict]:
+    po = PackageObj(payload=get_sensor_sample(sensor_id=sensor_id))
+    return sensor_id, po.to_dict()
